baf6bec00d0d382cad519a5e29a80943680eb683
dong2ast <kdh01132006@naver.com>
2023-09-05 19:22:58 +0900

    feat: 기본 세팅

---
+++ src/main/java/com/uspray/uspray/UsprayApplication.java
@@ -0,0 +1,13 @@
+package com.uspray.uspray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsprayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsprayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uspray/uspray/UsprayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uspray.uspray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsprayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
