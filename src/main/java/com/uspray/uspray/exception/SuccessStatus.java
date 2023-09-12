01e7c307e51a984e1313785122ef7dee9a7e1098
dong2ast <kdh01132006@naver.com>
2023-09-12 21:17:02 +0900

    feat: Http 프로토콜 응답 Enum 파일 정의

---
+++ src/main/java/com/uspray/uspray/exception/SuccessStatus.java
@@ -0,0 +1,26 @@
+package com.uspray.uspray.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessStatus {
+
+    /**
+     * 200 OK
+     */
+    LOGIN_SUCCESS(HttpStatus.OK, "로그인에 성공했습니다."),
+
+    /*
+     * 201 created
+     */
+    SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 완료되었습니다."),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
