88f4c4392d75d7689cfd21986fa21da0cd545d22
dong2ast <kdh01132006@naver.com>
2023-09-19 20:09:42 +0900

    feat: Not Found 예외처리 추가

---
+++ src/main/java/com/uspray/uspray/exception/model/CustomException.java
@@ -0,0 +1,18 @@
+package com.uspray.uspray.exception.model;
+
+import com.uspray.uspray.exception.ErrorStatus;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final ErrorStatus errorStatus;
+
+    public CustomException(ErrorStatus errorStatus, String message) {
+        super(message);
+        this.errorStatus = errorStatus;
+    }
+
+    public Integer getHttpStatus() {
+        return errorStatus.getHttpStatusCode();
+    }
+}
