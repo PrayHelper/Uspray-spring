1218b914e01a5adcad4fb0bde02ad5d08a28d851
baebae02 <bae4614@gmail.com>
2023-09-23 01:30:36 +0900

    fix: pray controller error 수정

--- src/main/java/com/uspray/uspray/controller/PrayController.java
+++ src/main/java/com/uspray/uspray/controller/PrayController.java
@@ -11,60 +11,65 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
+@RestController
 @RequestMapping("/pray")
 @Tag(name = "Pray", description = "기도제목 API")
-@SecurityRequirement(name = "JWT Auth")
+@RequiredArgsConstructor
 public class PrayController {
-    private final PrayService prayService;
 
-    @Operation(summary = "기도제목 목록 조회")
-    @ApiResponse(
-            responseCode = "200",
-            description = "기도제목 목록 반환",
-            content = @Content(schema = @Schema(implementation = PrayDto.class)))
-    @GetMapping()
-    public ApiResponseDto<PrayDto> getPrayList() {
-        return ApiResponseDto.success(SuccessStatus.GET_PRAY_LIST_SUCCESS, null);
-    }
+  private final PrayService prayService;
 
-    @GetMapping("/{prayId}")
-    @ApiResponse(
-            responseCode = "200",
-            description = "기도제목 조회",
-            content = @Content(schema = @Schema(implementation = PrayDto.class)))
-    @Operation(summary = "기도제목 조회")
-    public ApiResponseDto<PrayDto> getPrayDetail(
-            @Parameter(description = "기도제목 ID", required = true) Long prayId
-    ) {
-        return ApiResponseDto.success(SuccessStatus.GET_PRAY_SUCCESS, null);
-    }
+  @Operation(summary = "기도제목 목록 조회")
+  @ApiResponse(
+      responseCode = "200",
+      description = "기도제목 목록 반환",
+      content = @Content(schema = @Schema(implementation = PrayDto.class)))
+  @GetMapping()
+  public ApiResponseDto<PrayDto> getPrayList() {
+    return ApiResponseDto.success(SuccessStatus.GET_PRAY_LIST_SUCCESS, null);
+  }
 
-    @PostMapping()
-    @ApiResponse(
-            responseCode = "201",
-            description = "기도제목 생성",
-            content = @Content(schema = @Schema(implementation = PrayDto.class)))
-    @Operation(summary = "기도제목 생성")
-    public ApiResponseDto<PrayDto> createPray(
-          @RequestBody @Valid PrayRequestDto prayRequestDto
-    ) {
-        return ApiResponseDto.success(SuccessStatus.CREATE_PRAY_SUCCESS, prayService.createPray(prayRequestDto));
-    }
+  @GetMapping("/{prayId}")
+  @ApiResponse(
+      responseCode = "200",
+      description = "기도제목 조회",
+      content = @Content(schema = @Schema(implementation = PrayDto.class)))
+  @Operation(summary = "기도제목 조회")
+  public ApiResponseDto<PrayDto> getPrayDetail(
+      @Parameter(description = "기도제목 ID", required = true) Long prayId
+  ) {
+    return ApiResponseDto.success(SuccessStatus.GET_PRAY_SUCCESS, null);
+  }
 
-    @DeleteMapping("/{prayId}")
-    @ApiResponse(responseCode = "204", description = "기도제목 삭제")
-    @Operation(summary = "기도제목 삭제")
-    public ApiResponseDto<String> deletePray(
-            @Parameter(description = "기도제목 ID", required = true) Long prayId
-    ) {
-        return ApiResponseDto.success(SuccessStatus.DELETE_PRAY_SUCCESS, null);
-    }
+  @PostMapping()
+  @ApiResponse(
+      responseCode = "201",
+      description = "기도제목 생성",
+      content = @Content(schema = @Schema(implementation = PrayDto.class)))
+  @Operation(summary = "기도제목 생성")
+  public ApiResponseDto<PrayDto> createPray(
+      @RequestBody @Valid PrayRequestDto prayRequestDto
+  ) {
+    return ApiResponseDto.success(SuccessStatus.CREATE_PRAY_SUCCESS,
+        prayService.createPray(prayRequestDto));
+  }
+
+  @DeleteMapping("/{prayId}")
+  @ApiResponse(responseCode = "204", description = "기도제목 삭제")
+  @Operation(summary = "기도제목 삭제")
+  public ApiResponseDto<String> deletePray(
+      @Parameter(description = "기도제목 ID", required = true) Long prayId
+  ) {
+    return ApiResponseDto.success(SuccessStatus.DELETE_PRAY_SUCCESS, null);
+  }
 }
