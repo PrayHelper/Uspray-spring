f40250a6d797299d6f1699536695f2b7ace36c8f
Sion Shin <kmicety1@gmail.com>
2023-09-27 21:59:23 +0900

    chore: @SpringBootTest 주석

--- src/test/java/com/uspray/uspray/UsprayApplicationTests.java
+++ src/test/java/com/uspray/uspray/UsprayApplicationTests.java
@@ -3,7 +3,7 @@ package com.uspray.uspray;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UsprayApplicationTests {
 
 	@Test
