72823169b17e6aaf48f7fe1fd703b3bf88f08253
dong2ast <kdh01132006@naver.com>
2023-09-27 22:24:09 +0900

    feat: 아침 8시마다 알림 보내는 스케줄러 구현

--- src/main/java/com/uspray/uspray/Enums/NotificationType.java
+++ src/main/java/com/uspray/uspray/Enums/NotificationType.java
@@ -7,9 +7,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum NotificationType {
-    PRAY_TIME("기도 시간: 오전 8시"),
-    PRAY_FOR_ME("다른 사람이 내 기도 제목을 기도 했을 때"),
-    SHARED_MY_PRAY("다른 사람이 내 기도 제목을 공유 받았을 때")
+    PRAY_TIME("기도합시다~", "기도 시간: 오전 8시"),
+    PRAY_FOR_ME("기도받았다~", "다른 사람이 내 기도 제목을 기도 했을 때"),
+    SHARED_MY_PRAY("공유~", "다른 사람이 내 기도 제목을 공유 받았을 때")
     ;
-    private final String message;
+    private final String title;
+    private final String body;
 }
