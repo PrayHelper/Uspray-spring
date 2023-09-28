12fddcb0402c36b32c08c33f24719ff1581d21a9
Sion Shin <kmicety1@gmail.com>
2023-09-28 13:46:38 +0900

    feat: SharedPray 엔티티 추가

---
+++ src/main/java/com/uspray/uspray/domain/SharedPray.java
@@ -0,0 +1,43 @@
+package com.uspray.uspray.domain;
+
+import com.uspray.uspray.common.domain.AuditingTimeEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SharedPray extends AuditingTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shared_pray_id")
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pray_id")
+    private Pray pray;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public SharedPray(Pray pray, Member member) {
+        this.pray = pray;
+        this.member = member;
+    }
+}
