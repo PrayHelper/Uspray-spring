4d8f765c3124675835950104be22914791b58bbd
baebae02 <bae4614@gmail.com>
2023-10-03 00:01:51 +0900

    refactor: add method to PrayRepositoryCustom and override method

--- src/main/java/com/uspray/uspray/infrastructure/PrayRepositoryCustom.java
+++ src/main/java/com/uspray/uspray/infrastructure/PrayRepositoryCustom.java
@@ -1,5 +1,10 @@
 package com.uspray.uspray.infrastructure;
 
+import com.uspray.uspray.domain.Pray;
+import java.util.List;
+
 public interface PrayRepositoryCustom {
+
+  List<Pray> findAllWithOrder(String orderType, String username);
 
 }
