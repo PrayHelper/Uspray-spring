4f0a268d9ea4518bae07c655739538a72d4a5586
baebae02 <bae4614@gmail.com>
2023-10-02 23:57:12 +0900

    refactor: delete unused method

--- src/main/java/com/uspray/uspray/infrastructure/PrayRepository.java
+++ src/main/java/com/uspray/uspray/infrastructure/PrayRepository.java
@@ -3,17 +3,11 @@ package com.uspray.uspray.infrastructure;
 import com.uspray.uspray.domain.Pray;
 import com.uspray.uspray.exception.ErrorStatus;
 import com.uspray.uspray.exception.model.NotFoundException;
-import io.lettuce.core.dynamic.annotation.Param;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PrayRepository extends JpaRepository<Pray, Long> {
-
-  @Query("SELECT p FROM Pray p JOIN p.member m WHERE m.userId = :userId")
-  List<Pray> findAllByUserId(@Param("userId") String userId);
 
   default Pray getPrayById(Long id) {
     return findById(id).orElseThrow(
