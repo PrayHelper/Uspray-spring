56588ba314389eea74260cc143fc64880e6c7201
dong2ast <31067658+dong2ast@users.noreply.github.com>
2023-10-04 23:02:00 +0900

    Update src/main/java/com/uspray/uspray/DTO/auth/request/MemberDeleteDto.java
    MemberDeleteDto 스키마 설정 수정
    
    Co-authored-by: Sion Shin <82511301+Sion99@users.noreply.github.com>

--- src/main/java/com/uspray/uspray/DTO/auth/request/MemberDeleteDto.java
+++ src/main/java/com/uspray/uspray/DTO/auth/request/MemberDeleteDto.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 public class MemberDeleteDto {
 
-    @Schema(example = "[DONT_NEED, USE_DIFFICULT]", type = "array")
+    @Schema(example = "[\"DONT_NEED\", \"USE_DIFFICULT\"]", type = "array")
     private List<WithdrawReason> withdrawReason;
     @Schema(example = "다음에 올게요")
     private String reason;
