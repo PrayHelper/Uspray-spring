36c829872b0aa082600ab9a979d6d50485a1f5d4
dong2ast <31067658+dong2ast@users.noreply.github.com>
2023-10-05 10:56:04 +0900

    Feat/#29 member delete (#35)
    * feat: 회원 탈퇴 사유 관리 엔티티 생성
    
    * chore: 엔티티 네이밍 변경
    
    * modify: 회원 탈퇴 메서드 내부 처리 방식 변경
    
    * Update src/main/java/com/uspray/uspray/DTO/auth/request/MemberDeleteDto.java
    
    MemberDeleteDto 스키마 설정 수정
    
    Co-authored-by: Sion Shin <82511301+Sion99@users.noreply.github.com>
    
    * chore: WithdrawReason 필드 명 변경
    
    * modify: 불필요한 조회 연산 제거
    
    * chore: member 탈퇴 관련 도메인 필드 이름 변경
    
    ---------
    
    Co-authored-by: Sion Shin <82511301+Sion99@users.noreply.github.com>

---
+++ src/main/java/com/uspray/uspray/DTO/auth/request/MemberDeleteDto.java
@@ -0,0 +1,16 @@
+package com.uspray.uspray.DTO.auth.request;
+
+import com.uspray.uspray.Enums.WithdrawReason;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class MemberDeleteDto {
+
+    @Schema(example = "[\"DONT_NEED\", \"USE_DIFFICULT\"]", type = "array")
+    private List<WithdrawReason> withdrawReason;
+    @Schema(example = "다음에 올게요")
+    private String description;
+
+}
