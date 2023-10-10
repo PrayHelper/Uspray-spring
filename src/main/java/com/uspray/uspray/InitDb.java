29b635b2321d000efe42bdc82d9763c36fe2e67d
dong2ast <kdh01132006@naver.com>
2023-10-10 09:58:20 +0900

    feat: 더미데이터 추가

--- src/main/java/com/uspray/uspray/InitDb.java
+++ src/main/java/com/uspray/uspray/InitDb.java
@@ -1,8 +1,12 @@
 package com.uspray.uspray;
 
+import com.uspray.uspray.domain.Member;
+import com.uspray.uspray.domain.Pray;
+import java.time.LocalDate;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class InitDb {
 
     private final InitService initService;
-
     @PostConstruct
     public void init() {
         initService.dbInit();
@@ -23,10 +26,27 @@ public class InitDb {
     static class InitService {
 
         private final EntityManager em;
+        private final PasswordEncoder passwordEncoder;
 
         @Transactional
         public void dbInit() {
+            Member member = Member.builder()
+                .userId("test")
+                .password(passwordEncoder.encode("test"))
+                .name("홍길동")
+                .phone("01012345678")
+                .birth("2002-02-01")
+                .gender("female")
+                .build();
+            em.persist(member);
 
+            Pray pray = Pray.builder()
+                .content("테스트 기도")
+                .deadline(LocalDate.parse("2025-01-01"))
+                .member(member)
+                .build();
+
+            em.persist(pray);
         }
 
     }
