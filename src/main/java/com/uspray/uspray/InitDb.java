09f9983c0eb5a95bd666b1de336da6dabfd7a9bf
dong2ast <kdh01132006@naver.com>
2023-10-10 09:51:41 +0900

    feat: initDB 초기 설정

--- src/main/java/com/uspray/uspray/InitDb.java
+++ src/main/java/com/uspray/uspray/InitDb.java
@@ -1,5 +1,33 @@
 package com.uspray.uspray;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
 public class InitDb {
 
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+
+        @Transactional
+        public void dbInit() {
+
+        }
+
+    }
 }
