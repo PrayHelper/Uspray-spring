977f4ca4af4f208c2489f32386a270708f22c1a0
dong2ast <31067658+dong2ast@users.noreply.github.com>
2023-10-13 09:02:49 +0900

    Feat/#34 create dummy (#39)
    * feat: initDB 초기 설정
    
    * feat: 더미데이터 추가

--- src/main/java/com/uspray/uspray/InitDb.java
+++ src/main/java/com/uspray/uspray/InitDb.java
@@ -1,5 +1,53 @@
 package com.uspray.uspray;
 
+import com.uspray.uspray.domain.Member;
+import com.uspray.uspray.domain.Pray;
+import java.time.LocalDate;
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
 public class InitDb {
 
+    private final InitService initService;
+    @PostConstruct
+    public void init() {
+        initService.dbInit();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+        private final PasswordEncoder passwordEncoder;
+
+        @Transactional
+        public void dbInit() {
+            Member member = Member.builder()
+                .userId("test")
+                .password(passwordEncoder.encode("test"))
+                .name("홍길동")
+                .phone("01012345678")
+                .birth("2002-02-01")
+                .gender("female")
+                .build();
+            em.persist(member);
+
+            Pray pray = Pray.builder()
+                .content("테스트 기도")
+                .deadline(LocalDate.parse("2025-01-01"))
+                .member(member)
+                .build();
+
+            em.persist(pray);
+        }
+
+    }
 }
