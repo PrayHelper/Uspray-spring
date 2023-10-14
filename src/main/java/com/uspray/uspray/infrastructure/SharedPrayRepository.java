d85b97c02f1d5c3e2667e4e712e83e38c56dfeb2
Sion Shin <kmicety1@gmail.com>
2023-10-14 21:15:24 +0900

    feat: entitygraph 추가

--- src/main/java/com/uspray/uspray/infrastructure/SharedPrayRepository.java
+++ src/main/java/com/uspray/uspray/infrastructure/SharedPrayRepository.java
@@ -4,6 +4,7 @@ import com.uspray.uspray.DTO.pray.PrayDto;
 import com.uspray.uspray.domain.Member;
 import com.uspray.uspray.domain.SharedPray;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Repository;
 public interface SharedPrayRepository extends JpaRepository<SharedPray, Long> {
 
     // 수신자 기준 모두 찾기 (보관함 조회)
+    @EntityGraph(attributePaths = {"member"})
     List<SharedPray> findAllByMemberOrderByCreatedAtDesc(Member member);
 
 }
