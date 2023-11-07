2bc2cd679858f5df124e761c6e14eaf019e55d90
dong2ast <kdh01132006@naver.com>
2023-11-07 19:08:59 +0900

    ♻️ refactor: url 변수 카멜 case로 변경 + 업데이트 mapping PutMapping으로 변경

--- src/main/java/com/uspray/uspray/controller/GroupPrayController.java
+++ src/main/java/com/uspray/uspray/controller/GroupPrayController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,7 @@ public class GroupPrayController {
     }
 
     @Operation(summary = "모임 기도제목 수정")
-    @PostMapping("/update")
+    @PutMapping
     public ApiResponseDto<?> updateGroupPray(@RequestBody GroupPrayUpdateDto groupPrayUpdateDto) {
         groupPrayService.updateGroupPray(groupPrayUpdateDto);
         return ApiResponseDto.success(SuccessStatus.UPDATE_GROUP_PRAY_SUCCESS,
@@ -52,21 +53,21 @@ public class GroupPrayController {
     }
 
     @Operation(summary = "모임 기도제목 조회")
-    @GetMapping("/{group-id}")
+    @GetMapping("/{groupId}")
     @ApiResponse(
         responseCode = "200",
         description = "모임 기도제목 목록 반환",
         content = @Content(schema = @Schema(implementation = GroupPrayResponseDto.class)))
     public ApiResponseDto<List<GroupPrayResponseDto>> getGroupPray(
-        @PathVariable(name = "group-id") Long groupId,
+        @PathVariable(name = "groupId") Long groupId,
         @Parameter(hidden = true) @AuthenticationPrincipal User user) {
         return ApiResponseDto.success(SuccessStatus.GET_GROUP_PRAY_LIST_SUCCESS,
             groupPrayService.getGroupPray(groupId, user.getUsername()));
     }
 
     @Operation(summary = "모임 기도제목 삭제")
-    @DeleteMapping("/{grouppray-id}")
-    public ApiResponseDto<?> deleteGroupPray(@PathVariable(name = "grouppray-id") Long id) {
+    @DeleteMapping("/{groupPrayId}")
+    public ApiResponseDto<?> deleteGroupPray(@PathVariable(name = "groupPrayId") Long id) {
         groupPrayService.deleteGroupPray(id);
         return ApiResponseDto.success(SuccessStatus.DELETE_GROUP_PRAY_SUCCESS,
             SuccessStatus.DELETE_GROUP_PRAY_SUCCESS.getMessage());
