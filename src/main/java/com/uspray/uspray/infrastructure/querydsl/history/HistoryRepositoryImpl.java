5f4ba0e2ed530f8624d180fbceb439d2d6d5534e
Sion Shin <kmicety1@gmail.com>
2023-11-09 13:46:23 +0900

    refactor: BooleanExpression을 where절 분기로 변경

--- src/main/java/com/uspray/uspray/infrastructure/querydsl/history/HistoryRepositoryImpl.java
+++ src/main/java/com/uspray/uspray/infrastructure/querydsl/history/HistoryRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.uspray.uspray.infrastructure.querydsl.history;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.uspray.uspray.domain.History;
 import com.uspray.uspray.domain.QHistory;
@@ -23,43 +22,29 @@ public class HistoryRepositoryImpl implements HistoryRepositoryCustom {
     public Page<History> findBySearchOption(String username, String keyword, Boolean isPersonal,
         Boolean isShared, LocalDate startDate, LocalDate endDate, Pageable pageable) {
         QHistory history = QHistory.history;
-        BooleanExpression predicate = history.member.userId.eq(username);
 
-        if (keyword != null && !keyword.isEmpty()) {
-            predicate = predicate.and(history.content.containsIgnoreCase(keyword));
-        }
-
-        if (Boolean.TRUE.equals(isPersonal) && !Boolean.TRUE.equals(isShared)) {
-            // Only personal, originPrayID가 null인 것
-            predicate = predicate.and(history.originPrayId.isNull());
-        }
-
-        if (Boolean.TRUE.equals(isShared) && !Boolean.TRUE.equals(isPersonal)) {
-            // Only shared, originPrayId가 null이 아닌 것
-            predicate = predicate.and(history.originPrayId.isNotNull());
-        }
-
-        // 만약 둘 다 true(두 체크박스 모두 선택) 또는 false(두 체크박스 선택 안함) 라면, 아무 조건도 추가하지 않습니다.
-        // 즉, isPersonal과 isShared 둘 다 true, false인 경우 모든 결과를 포함합니다.
-
-        if (startDate != null) {
-            predicate = predicate.and(history.createdAt.before(endDate.atStartOfDay()));
-        }
-
-        if (endDate != null) {
-            predicate = predicate.and(history.deadline.after(startDate));
-        }
-
-        // fetchCount가 deprecated 됐다고 해서 추후 수정 예정
         long total = queryFactory
-            .select(history)
-            .from(history)
-            .where(predicate)
+            .selectFrom(history)
+            .where(
+                history.member.userId.eq(username),
+                keyword != null && !keyword.isEmpty() ? history.content.containsIgnoreCase(keyword) : null,
+                Boolean.TRUE.equals(isPersonal) && !Boolean.TRUE.equals(isShared) ? history.originPrayId.isNull() : null,
+                Boolean.TRUE.equals(isShared) && !Boolean.TRUE.equals(isPersonal) ? history.originPrayId.isNotNull() : null,
+                startDate != null ? history.createdAt.before(endDate.atStartOfDay()) : null,
+                endDate != null ? history.deadline.after(startDate) : null
+            )
             .fetchCount();
 
         List<History> results = queryFactory
             .selectFrom(history)
-            .where(predicate)
+            .where(
+                history.member.userId.eq(username),
+                keyword != null && !keyword.isEmpty() ? history.content.containsIgnoreCase(keyword) : null,
+                Boolean.TRUE.equals(isPersonal) && !Boolean.TRUE.equals(isShared) ? history.originPrayId.isNull() : null,
+                Boolean.TRUE.equals(isShared) && !Boolean.TRUE.equals(isPersonal) ? history.originPrayId.isNotNull() : null,
+                startDate != null ? history.createdAt.before(endDate.atStartOfDay()) : null,
+                endDate != null ? history.deadline.after(startDate) : null
+            )
             .offset(pageable.getOffset())
             .limit(pageable.getPageSize())
             .fetch();
