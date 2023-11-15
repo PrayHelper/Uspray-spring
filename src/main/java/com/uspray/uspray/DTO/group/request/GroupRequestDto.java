c179e8470dc2931308e37a3efa958e50c445a15a
Sion Shin <kmicety1@gmail.com>
2023-11-15 22:01:05 +0900

    feat: add validation to GroupResponseDto

--- src/main/java/com/uspray/uspray/DTO/group/request/GroupRequestDto.java
+++ src/main/java/com/uspray/uspray/DTO/group/request/GroupRequestDto.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class GroupRequestDto {
 
+        @NotNull(message = "모임 이름을 입력해주세요.")
         private String name;
-
-        private String description;
 }
