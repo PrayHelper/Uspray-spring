e0c22e42ab1791494ca703c500723267b8a2c5f0
Sion Shin <kmicety1@gmail.com>
2023-11-15 22:58:14 +0900

    feat: add changeGroupName, changeGroupLeader, kickGroupMember, deleteGroup

---
+++ src/main/java/com/uspray/uspray/DTO/group/request/GroupLeaderRequestDto.java
@@ -0,0 +1,16 @@
+package com.uspray.uspray.DTO.group.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupLeaderRequestDto {
+
+    @NotNull
+    private String username;
+}
