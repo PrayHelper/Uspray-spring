713e55b89d362254f490a729c92cbd648b3d78b2
Sion Shin <kmicety1@gmail.com>
2023-11-16 20:41:17 +0900

    feat: add validation to GroupRequestDto

--- src/main/java/com/uspray/uspray/DTO/group/request/GroupRequestDto.java
+++ src/main/java/com/uspray/uspray/DTO/group/request/GroupRequestDto.java
@@ -4,13 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class GroupRequestDto {
 
-        @NotNull(message = "모임 이름을 입력해주세요.")
+        @NotBlank(message = "모임 이름을 입력해주세요.")
+        @Size(min = 1, max = 15, message = "모임 이름은 공백 포함 15자 이내로 입력해주세요.")
         private String name;
 }
