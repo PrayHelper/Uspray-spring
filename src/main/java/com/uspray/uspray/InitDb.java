65007f17175bd56dc16dd5c18ddf431115d5fd55
Sion Shin <kmicety1@gmail.com>
2023-11-18 14:13:51 +0900

    fix: build connection between member and group

--- src/main/java/com/uspray/uspray/InitDb.java
+++ src/main/java/com/uspray/uspray/InitDb.java
@@ -48,6 +48,8 @@ public class InitDb {
                 .leader(member)
                 .name("테스트 모임")
                 .build();
+            group.addMember(member);
+            member.joinGroup(group);
             em.persist(group);
 
             Category category = Category.builder()
