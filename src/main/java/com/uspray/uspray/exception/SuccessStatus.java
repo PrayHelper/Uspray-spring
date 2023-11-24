42906c3c4b43810edc6bd302fd0a915503756024
baebae02 <bae4614@gmail.com>
2023-11-24 15:55:41 +0900

    feat: edit category order api

--- src/main/java/com/uspray/uspray/exception/SuccessStatus.java
+++ src/main/java/com/uspray/uspray/exception/SuccessStatus.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessStatus {
-    
+
     /**
      * 200 OK
      */
@@ -33,6 +33,7 @@ public enum SuccessStatus {
     GET_HISTORY_DETAIL_SUCCESS(HttpStatus.OK, "히스토리 상세 조회에 성공했습니다."),
     INCREASE_PRAY_COUNT_SUCCESS(HttpStatus.OK, "기도 횟수 추가에 성공했습니다."),
     COMPLETE_PRAY_SUCCESS(HttpStatus.OK, "기도제목 완료에 성공했습니다."),
+    GET_CATEGORY_LIST_SUCCESS(HttpStatus.OK, "카테고리 목록 조회에 성공했습니다."),
     /*
      * 201 created
      */
@@ -42,7 +43,7 @@ public enum SuccessStatus {
     CREATE_CATEGORY_SUCCESS(HttpStatus.CREATED, "카테고리 생성에 성공했습니다."),
     SHARE_PRAY_SUCCESS(HttpStatus.CREATED, "기도제목 공유에 성공했습니다."),
     SHARE_PRAY_AGREE_SUCCESS(HttpStatus.CREATED, "기도제목 공유 수락에 성공했습니다."),
-    
+
     /*
      * 204 deleted
      */
@@ -50,9 +51,9 @@ public enum SuccessStatus {
     DELETE_GROUP_PRAY_SUCCESS(HttpStatus.NO_CONTENT, "모임 기도제목 삭제에 성공했습니다."),
     WITHDRAWAL_SUCCESS(HttpStatus.NO_CONTENT, "회원 탈퇴에 성공했습니다."),
     DELETE_CATEGORY_SUCCESS(HttpStatus.NO_CONTENT, "카테고리 삭제에 성공했습니다.");
-    
-    
+
+
     private final HttpStatus httpStatus;
     private final String message;
-    
+
 }
