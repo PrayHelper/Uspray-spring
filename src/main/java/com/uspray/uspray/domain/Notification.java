0bd5655148447b08451ab6759f6904938b8367d9
baebae02 <bae4614@gmail.com>
2023-11-24 19:38:22 +0900

    Revert "feat: make notification domain"
    This reverts commit 3f8c61eda367e2a30d103440be12a1232cf7cf0b.

--- src/main/java/com/uspray/uspray/domain/Notification.java
+++ src/main/java/com/uspray/uspray/domain/Notification.java
@@ -2,13 +2,10 @@ package com.uspray.uspray.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
 public class Notification {
 
     private String title;
@@ -16,12 +13,4 @@ public class Notification {
     private String image;
     @Id
     private Long id;
-
-    @Builder
-    public Notification(String title, String body, String image, Long id) {
-        this.title = title;
-        this.body = body;
-        this.image = image;
-        this.id = id;
-    }
 }
