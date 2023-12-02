eabf1b7ae6302acafd242e4ff060a42dccae77b2
Sion Shin <kmicety1@gmail.com>
2023-12-02 15:34:44 +0900

    chore: add swagger description

--- src/main/java/com/uspray/uspray/controller/GroupController.java
+++ src/main/java/com/uspray/uspray/controller/GroupController.java
@@ -8,10 +8,16 @@ import com.uspray.uspray.DTO.group.response.GroupMemberResponseDto;
 import com.uspray.uspray.exception.SuccessStatus;
 import com.uspray.uspray.service.GroupFacade;
 import com.uspray.uspray.service.GroupService;
+import io.netty.channel.ChannelHandler;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +35,12 @@ public class GroupController {
     private final GroupService groupService;
     private final GroupFacade groupFacade;
 
+    @Operation(summary = "모임 목록 조회")
+    @ApiResponse(
+        responseCode = "200",
+        description = "사용자가 가입한 목록 조회",
+        content = @Content(schema = @Schema(implementation = GroupListResponseDto.class))
+    )
     @GetMapping
     public ApiResponseDto<GroupListResponseDto> getGroupList(
         @Parameter(hidden = true) @AuthenticationPrincipal User user) {
@@ -36,6 +48,12 @@ public class GroupController {
             groupService.getGroupList(user.getUsername()));
     }
 
+    @Operation(summary = "모임 생성")
+    @ApiResponse(
+        responseCode = "201",
+        description = "신규로 모임을 생성합니다",
+        content = @Content(schema = @Schema(implementation = GroupRequestDto.class))
+    )
     @PostMapping
     public ApiResponseDto<?> createGroup(
         @Parameter(hidden = true) @AuthenticationPrincipal User user,
@@ -45,6 +63,12 @@ public class GroupController {
             SuccessStatus.CREATE_GROUP_SUCCESS.getMessage());
     }
 
+    @Operation(summary = "[모임 리더] 모임 이름을 변경합니다")
+    @ApiResponse(
+        responseCode = "200",
+        description = "모임 이름 변경",
+        content = @Content(schema = @Schema(implementation = GroupRequestDto.class))
+    )
     @PutMapping("/{groupId}/change-name")
     public ApiResponseDto<?> changeGroupName(
         @Parameter(hidden = true) @AuthenticationPrincipal User user,
@@ -55,6 +79,12 @@ public class GroupController {
             SuccessStatus.CHANGE_GROUP_NAME_SUCCESS.getMessage());
     }
 
+    @Operation(summary = "[모임 리더] 모임 리더를 위임합니다")
+    @ApiResponse(
+        responseCode = "200",
+        description = "모임 리더 위임",
+        content = @Content(schema = @Schema(implementation = GroupMemberRequestDto.class))
+    )
     @PutMapping("/{groupId}/change-leader")
     public ApiResponseDto<?> changeGroupLeader(
         @Parameter(hidden = true) @AuthenticationPrincipal User user,
@@ -65,6 +95,12 @@ public class GroupController {
             SuccessStatus.CHANGE_GROUP_LEADER_SUCCESS.getMessage());
     }
 
+    @Operation(summary = "[모임 리더] 회원 내보내기")
+    @ApiResponse(
+        responseCode = "200",
+        description = "모임 회원을 내보냅니다",
+        content = @Content(schema = @Schema(implementation = GroupMemberRequestDto.class))
+    )
     @DeleteMapping("/{groupId}/kick")
     public ApiResponseDto<?> kickGroupMember(
         @Parameter(hidden = true) @AuthenticationPrincipal User user,
@@ -75,6 +111,11 @@ public class GroupController {
             SuccessStatus.KICK_GROUP_MEMBER_SUCCESS.getMessage());
     }
 
+    @Operation(summary = "모임 가입하기")
+    @ApiResponse(
+        responseCode = "200",
+        description = "모임에 가입합니다"
+    )
     @PostMapping("/{groupId}/join")
     public ApiResponseDto<?> addGroupMember(
         @Parameter(hidden = true) @AuthenticationPrincipal User user,
@@ -84,6 +125,12 @@ public class GroupController {
             SuccessStatus.ADD_GROUP_MEMBER_SUCCESS.getMessage());
     }
 
+    @Operation(summary = "모임 탈퇴하기")
+    @ApiResponse(
+        responseCode = "204",
+        description = "모임을 떠납니다",
+        content = @Content(schema = @Schema(implementation = GroupMemberRequestDto.class))
+    )
     @DeleteMapping("/{groupId}/leave")
     public ApiResponseDto<?> leaveGroup(
         @Parameter(hidden = true) @AuthenticationPrincipal User user,
@@ -93,6 +140,11 @@ public class GroupController {
             SuccessStatus.LEAVE_GROUP_SUCCESS.getMessage());
     }
 
+    @Operation(summary = "[모임 리더] 모임 삭제하기")
+    @ApiResponse(
+        responseCode = "200",
+        description = "모임을 삭제합니다. 모임 회원, 모임 기도제목 등 일괄 삭제"
+    )
     @DeleteMapping("/{groupId}")
     public ApiResponseDto<?> deleteGroup(
         @Parameter(hidden = true) @AuthenticationPrincipal User user,
@@ -102,6 +154,11 @@ public class GroupController {
             SuccessStatus.DELETE_GROUP_SUCCESS.getMessage());
     }
 
+    @Operation(summary = "회원 검색")
+    @ApiResponse(
+        responseCode = "200",
+        description = "모임 내 회원 목록을 조회합니다"
+    )
     @GetMapping("/{groupId}/member/search")
     public ApiResponseDto<List<GroupMemberResponseDto>> searchGroupMembers(
         @PathVariable Long groupId,
