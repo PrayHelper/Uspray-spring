bd4d02aa7b444967b775de59e444a15cd25b2a98
Sion Shin <kmicety1@gmail.com>
2023-12-12 09:28:38 +0400

    refactor: add exception handler in Login service

--- src/main/java/com/uspray/uspray/exception/ErrorStatus.java
+++ src/main/java/com/uspray/uspray/exception/ErrorStatus.java
@@ -25,6 +25,7 @@ public enum ErrorStatus {
     ALREADY_SHARED_EXCEPTION(HttpStatus.BAD_REQUEST, "이미 공유된 기도제목입니다."),
     ALREADY_EXIST_GROUP_NAME_EXCEPTION(HttpStatus.BAD_REQUEST, "이미 존재하는 모임 이름입니다."),
     ALREADY_CANCEL_EXCEPTION(HttpStatus.BAD_REQUEST, "이미 취소된 기도제목입니다."),
+    WRONG_LOGIN_INFO_EXCEPTION(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 틀렸습니다."),
 
     /*
      * 401 UNAUTHORIZED
@@ -35,6 +36,7 @@ public enum ErrorStatus {
     DELETE_NOT_AUTHORIZED_EXCEPTION(HttpStatus.UNAUTHORIZED, "기도제목을 삭제할 권한이 없습니다."),
     CATEGORY_UNAUTHORIZED_EXCEPTION(HttpStatus.UNAUTHORIZED, "해당 카테고리에 대한 권한이 없습니다."),
     GROUP_UNAUTHORIZED_EXCEPTION(HttpStatus.UNAUTHORIZED, "해당 모임에 대한 권한이 없습니다."),
+    REFRESH_TOKEN_NOT_VALID_EXCEPTION(HttpStatus.UNAUTHORIZED, "유효하지 않는 리프레시 토큰입니다."),
 
     /**
      * 404 NOT FOUND
@@ -48,7 +50,12 @@ public enum ErrorStatus {
     HISTORY_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 히스토리를 찾을 수 없습니다."),
     CATEGORY_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 카테고리를 찾을 수 없습니다."),
     GROUP_MEMBER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 멤버를 모임에서 찾을 수 없습니다."),
-    NOT_FOUND_GROUP_MEMBER_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 모임 회원입니다.");
+    NOT_FOUND_GROUP_MEMBER_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 모임 회원입니다."),
+
+    /**
+     * 500 INTERNAL SERVER ERROR
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 요청을 처리할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
