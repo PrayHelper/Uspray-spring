705a1cc16983904a4ad42740cf91ade45609af3c
dong2ast <kdh01132006@naver.com>
2023-12-22 21:34:55 +0900

    fix: QueryDSL 오류 수정

--- src/main/java/com/uspray/uspray/infrastructure/querydsl/grouppray/GroupPrayRepositoryImpl.java
+++ src/main/java/com/uspray/uspray/infrastructure/querydsl/grouppray/GroupPrayRepositoryImpl.java
@@ -1,51 +1,40 @@
 package com.uspray.uspray.infrastructure.querydsl.grouppray;
 
-import static com.uspray.uspray.domain.QGroupPray.groupPray;
-import static com.uspray.uspray.domain.QScrapAndHeart.scrapAndHeart;
-
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.uspray.uspray.DTO.grouppray.GroupPrayResponseDto;
-import com.uspray.uspray.DTO.grouppray.QGroupPrayResponseDto;
-import java.time.LocalDate;
-import java.util.List;
-import javax.persistence.EntityManager;
-
 //TODO 리팩터링하거나 지우거나
-public class GroupPrayRepositoryImpl implements GroupPrayRepositoryCustom {
+public class GroupPrayRepositoryImpl {
 
-    private final JPAQueryFactory queryFactory;
-
-    public GroupPrayRepositoryImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
-    }
+//    private final JPAQueryFactory queryFactory;
+//
+//    public GroupPrayRepositoryImpl(EntityManager em) {
+//        this.queryFactory = new JPAQueryFactory(em);
+//    }
 
     //Group Id, memberId, Pray중 Shared만
-    @Override
-    public List<GroupPrayResponseDto> getGroupPrayList(Long groupId, String userId) {
-        return queryFactory
-            .select(new QGroupPrayResponseDto(
-                groupPray.id,
-                groupPray.content,
-                groupPray.author.name,
-                groupPray.author.id,
-                scrapAndHeart.member.id.coalesce(-1L).as(scrapAndHeart.member.id),
-                scrapAndHeart.heart.coalesce(false).as(scrapAndHeart.heart),
-                scrapAndHeart.scrap.coalesce(false).as(scrapAndHeart.scrap),
-                groupPray.createdAt))
-            .from(groupPray)
-            .leftJoin(groupPray.scrapAndHeart, scrapAndHeart)
-            .on(scarpAndHeartEq(userId))
-            .where(groupPray.group.id.eq(groupId),
-                groupPray.deadline.gt(LocalDate.now()))
-            .orderBy()
-            .fetch();
-    }
-
-    private BooleanExpression scarpAndHeartEq(String userId) {
-        return queryFactory
-            .selectFrom(scrapAndHeart)
-            .where(scrapAndHeart.member.userId.eq(userId))
-            .fetch().size() == 0 ? null : scrapAndHeart.member.userId.eq(userId);
-    }
+//    @Override
+//    public List<GroupPrayResponseDto> getGroupPrayList(Long groupId, String userId) {
+//        return queryFactory
+//            .select(new QGroupPrayResponseDto(
+//                groupPray.id,
+//                groupPray.content,
+//                groupPray.author.name,
+//                groupPray.author.id,
+//                scrapAndHeart.member.id.coalesce(-1L).as(scrapAndHeart.member.id),
+//                scrapAndHeart.heart.coalesce(false).as(scrapAndHeart.heart),
+//                scrapAndHeart.scrap.coalesce(false).as(scrapAndHeart.scrap),
+//                groupPray.createdAt))
+//            .from(groupPray)
+//            .leftJoin(groupPray.scrapAndHeart, scrapAndHeart)
+//            .on(scarpAndHeartEq(userId))
+//            .where(groupPray.group.id.eq(groupId),
+//                groupPray.deadline.gt(LocalDate.now()))
+//            .orderBy()
+//            .fetch();
+//    }
+//
+//    private BooleanExpression scarpAndHeartEq(String userId) {
+//        return queryFactory
+//            .selectFrom(scrapAndHeart)
+//            .where(scrapAndHeart.member.userId.eq(userId))
+//            .fetch().size() == 0 ? null : scrapAndHeart.member.userId.eq(userId);
+//    }
 }
