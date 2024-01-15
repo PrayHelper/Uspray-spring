f6546428508077244c6755df5f1b02b2ccad21b7
baebae02 <bae4614@gmail.com>
2024-01-15 08:32:34 -0600

    fix: use prayResponseDto of in get pray list

--- src/main/java/com/uspray/uspray/infrastructure/querydsl/category/CategoryRepositoryImpl.java
+++ src/main/java/com/uspray/uspray/infrastructure/querydsl/category/CategoryRepositoryImpl.java
@@ -3,13 +3,14 @@ package com.uspray.uspray.infrastructure.querydsl.category;
 import static com.uspray.uspray.domain.QCategory.category;
 import static com.uspray.uspray.domain.QPray.pray;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.uspray.uspray.DTO.pray.PrayListResponseDto;
 import com.uspray.uspray.DTO.pray.response.PrayResponseDto;
 import com.uspray.uspray.domain.Category;
+import com.uspray.uspray.domain.Pray;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -32,25 +33,20 @@ public class CategoryRepositoryImpl implements CategoryRepositoryCustom {
         // 각 카테고리 별로 PrayResponseDto 목록 가져오기
         List<PrayListResponseDto> prayListResponseDtos = new ArrayList<>();
         for (Category cat : categories) {
-            List<PrayResponseDto> prays = queryFactory
-                .select(Projections.constructor(
-                    PrayResponseDto.class,
-                    pray.id,
-                    pray.content,
-                    pray.member.name,
-                    pray.deadline,
-                    pray.category.id,
-                    pray.category.name,
-                    pray.lastPrayedAt,
-                    pray.isShared))
-                .from(pray)
+            List<Pray> prays = queryFactory
+                .selectFrom(pray)
                 .where(pray.category.id.eq(cat.getId())
                     .and(pray.member.userId.eq(username))
                     .and(pray.prayType.stringValue().likeIgnoreCase(prayType)))
                 .fetch();
 
+            List<PrayResponseDto> prayResponseDtos = prays.stream()
+                .map(PrayResponseDto::of)
+                .collect(Collectors.toList());
+
             prayListResponseDtos.add(
-                new PrayListResponseDto(cat.getId(), cat.getName(), cat.getColor(), prays));
+                new PrayListResponseDto(cat.getId(), cat.getName(), cat.getColor(),
+                    prayResponseDtos));
         }
         return prayListResponseDtos;
     }
