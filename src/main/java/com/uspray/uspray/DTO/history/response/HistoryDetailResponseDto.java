af360e2b6410d987009a39dc2a021d55e78f0853
baebae02 <bae4614@gmail.com>
2024-01-19 12:06:55 -0600

    feat: add canEdit at HistoryDetailResponseDto

--- src/main/java/com/uspray/uspray/DTO/history/response/HistoryDetailResponseDto.java
+++ src/main/java/com/uspray/uspray/DTO/history/response/HistoryDetailResponseDto.java
@@ -1,5 +1,6 @@
 package com.uspray.uspray.DTO.history.response;
 
+import com.uspray.uspray.Enums.PrayType;
 import com.uspray.uspray.domain.History;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
@@ -27,10 +28,13 @@ public class HistoryDetailResponseDto {
 
     private Long categoryId;
 
+    private Boolean canEdit;
+
     public static HistoryDetailResponseDto of(History history) {
         return new HistoryDetailResponseDto(history.getId(), history.getMember().getUserId(),
             history.getContent(), history.getPersonalCount(), history.getTotalCount(),
             history.getDeadline(),
-            history.getCreatedAt().toLocalDate(), history.getCategoryId());
+            history.getCreatedAt().toLocalDate(), history.getCategoryId(),
+            history.getPrayType() == PrayType.PERSONAL && !history.getIsShared());
     }
 }
