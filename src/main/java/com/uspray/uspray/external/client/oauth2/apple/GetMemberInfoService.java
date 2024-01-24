caf360eb287490870c99e6367dce9f5e5795b0c7
dong2ast <kdh01132006@naver.com>
2024-01-24 14:58:50 +0900

    chore: apple login token log

--- src/main/java/com/uspray/uspray/external/client/oauth2/apple/GetMemberInfoService.java
+++ src/main/java/com/uspray/uspray/external/client/oauth2/apple/GetMemberInfoService.java
@@ -33,6 +33,8 @@ public class GetMemberInfoService {
             authorizationCode
         ).getIdToken();
 
+        System.out.println(idToken);
+
         return TokenDecoder.decodePayload(idToken, AppleIdTokenPayload.class);
     }
 
