03fc890e9cf83ad2bede92ff969baae21fcaff12
baebae02 <bae4614@gmail.com>
2024-02-19 05:00:41 -0600

    feat: pray table sql

--- src/main/java/com/uspray/uspray/config/sql/pray.sql
+++ src/main/java/com/uspray/uspray/config/sql/pray.sql
@@ -1,35 +1,37 @@
 -- MERGE STORAGE TABLE AND PRAY TABLE TO PRAY TABLE
 ALTER TABLE pray RENAME TO temp_pray;
 ALTER TABLE storage RENAME TO pray;
-ALTER TABLE pray RENAME COLUMN id TO pray_id;
 
 -- ADD COLUMN
-ALTER TABLE pray ADD COLUMN original_pray_id BIGINT;
-UPDATE TABLE SET origin_pray_id = pray_id;
+ALTER TABLE pray RENAME COLUMN pray_id TO origin_pray_id;
+ALTER TABLE pray ALTER COLUMN origin_pray_id TYPE BIGINT;
+ALTER TABLE pray RENAME COLUMN id TO pray_id;
+ALTER TABLE pray ALTER COLUMN pray_id TYPE BIGINT;
+
 ALTER TABLE pray RENAME COLUMN pray_cnt to count;
-ALTER TABLE pray ALTER COLUMN deadline TYPE DATE USING to_date(deadline, 'YYYY-MM-DD');
+ALTER TABLE pray ALTER COLUMN deadline TYPE DATE USING (deadline::DATE);
 ALTER TABLE pray ADD COLUMN deleted BOOLEAN;
 UPDATE pray SET deleted = (deleted_at IS NOT NULL);
 ALTER TABLE pray DROP COLUMN deleted_at;
 ALTER TABLE pray ADD COLUMN last_prayed_at DATE;
-ALTER TABLE pray ADD COLUMN updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE pray ADD COLUMN updated_at TIMESTAMP;
 ALTER TABLE pray ADD COLUMN pray_type VARCHAR(255);
 
 -- CREATE CONTENT
 ALTER TABLE pray ADD COLUMN content VARCHAR(255);
 ALTER TABLE pray ADD COLUMN is_shared BOOLEAN;
 UPDATE pray AS p
-SET content = tp.title,
+SET content = LEFT(tp.title, 255),
     is_shared = tp.is_shared
     FROM temp_pray AS tp
-AND p.pray_id = tp.id;
-ALTER TABLE pray DROP COLUMN title;
--- ALTER TABLE pray DROP COLUMN target;
+WHERE p.pray_id = tp.id;
+-- ALTER TABLE temp_pray DROP COLUMN title;
+-- ALTER TABLE temp_pray DROP COLUMN target;
 
 -- HANDLE COMPLETE PRAY
 UPDATE pray
-SET last_prayed_at = TO_DATE(c.created_at)
-    FROM complete AS c
+SET last_prayed_at = c.created_at::date
+FROM complete AS c
 WHERE pray.pray_id = c.storage_id;
 
 -- HANDLE PRAY TYPE
@@ -37,4 +39,11 @@ UPDATE pray
 SET pray_type = CASE
     WHEN is_shared THEN 'shared'
     ELSE 'personal'
+    END;
+
+-- SET UPDATED_AT
+UPDATE pray
+SET updated_at = CASE
+    WHEN last_prayed_at IS NOT NULL THEN last_prayed_at
+    ELSE created_at
     END;
