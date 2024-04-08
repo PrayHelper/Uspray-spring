d7bc2282b09ed0e17f40743a4b27518dcbd7992a
baebae02 <bae4614@gmail.com>
2024-04-07 21:19:11 -0500

    feat: MemberRepository Test 추가

--- src/test/java/com/uspray/uspray/UsprayApplicationTests.java
+++ src/test/java/com/uspray/uspray/UsprayApplicationTests.java
@@ -3,11 +3,12 @@ package com.uspray.uspray;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
+//@Testcontainers
 class UsprayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
---
+++ src/test/java/com/uspray/uspray/repository/RepositoryTest.java
@@ -0,0 +1,32 @@
+package com.uspray.uspray.repository;
+
+import com.uspray.uspray.domain.Member;
+import com.uspray.uspray.infrastructure.CategoryRepository;
+import com.uspray.uspray.infrastructure.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+//@DataJpaTest
+@Transactional
+@SpringBootTest
+public abstract class RepositoryTest {
+
+    @Autowired
+    protected MemberRepository memberRepository;
+
+    @Autowired
+    protected CategoryRepository categoryRepository;
+
+    @BeforeEach
+    void userSetup() {
+        Member member = memberRepository.save(Member.builder()
+            .userId("leesunshin")
+            .password("test")
+            .name("이순신")
+            .phone("01011111111")
+            .build());
+    }
+
+}
