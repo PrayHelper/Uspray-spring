052c29aa3ff476d6577c13c09bd21b2ab680c0a9
baebae02 <bae4614@gmail.com>
2024-04-07 21:25:05 -0500

    feat: test에 activeprofile 추가

--- src/test/java/com/uspray/uspray/common/MemberTest.java
+++ src/test/java/com/uspray/uspray/common/MemberTest.java
@@ -7,10 +7,12 @@ import com.uspray.uspray.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
+@ActiveProfiles("set3")
 public abstract class MemberTest {
 
     @Autowired
--- src/test/java/com/uspray/uspray/repository/RepositoryTest.java
+++ src/test/java/com/uspray/uspray/repository/RepositoryTest.java
@@ -6,11 +6,13 @@ import com.uspray.uspray.infrastructure.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 //@DataJpaTest
 @Transactional
 @SpringBootTest
+@ActiveProfiles("set3")
 public abstract class RepositoryTest {
 
     @Autowired
