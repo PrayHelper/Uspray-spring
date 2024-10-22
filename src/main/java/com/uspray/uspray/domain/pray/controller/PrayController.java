300cfe7a3c853a6bd6dcfd8b40f74e9373d8daa6
조수아 <suacho0724@gmail.com>
2024-10-22 17:06:16 +0900

    refactor: prayController indent 수정 및 pathvariable 누락 해결

--- src/main/java/com/uspray/uspray/domain/pray/controller/PrayController.java
+++ src/main/java/com/uspray/uspray/domain/pray/controller/PrayController.java
@@ -1,6 +1,5 @@
 package com.uspray.uspray.domain.pray.controller;
 
-
 import com.uspray.uspray.domain.pray.dto.pray.PrayListResponseDto;
 import com.uspray.uspray.domain.pray.dto.pray.request.PrayRequestDto;
 import com.uspray.uspray.domain.pray.dto.pray.request.PrayUpdateRequestDto;
@@ -28,79 +27,78 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PrayController implements PrayApi {
 
-    private final PrayService prayService;
-    private final PrayFacade prayFacade;
+	private final PrayService prayService;
+	private final PrayFacade prayFacade;
 
-    @GetMapping
-    public ApiResponseDto<List<PrayListResponseDto>> getPrayList(
-        @AuthenticationPrincipal User user,
-        String prayType
-    ) {
-        return ApiResponseDto.success(SuccessStatus.GET_PRAY_LIST_SUCCESS,
-            prayFacade.getPrayList(user.getUsername(), prayType));
-    }
+	@GetMapping
+	public ApiResponseDto<List<PrayListResponseDto>> getPrayList(
+		@AuthenticationPrincipal User user,
+		String prayType
+	) {
+		return ApiResponseDto.success(SuccessStatus.GET_PRAY_LIST_SUCCESS,
+			prayFacade.getPrayList(user.getUsername(), prayType));
+	}
 
-    @GetMapping("/{prayId}")
-    public ApiResponseDto<PrayResponseDto> getPrayDetail(
-        @AuthenticationPrincipal User user,
-        Long prayId
-    ) {
-        return ApiResponseDto.success(SuccessStatus.GET_PRAY_SUCCESS,
-            prayService.getPrayDetail(prayId, user.getUsername()));
-    }
+	@GetMapping("/{prayId}")
+	public ApiResponseDto<PrayResponseDto> getPrayDetail(
+		@AuthenticationPrincipal User user,
+		@PathVariable("prayId") Long prayId) {
+		return ApiResponseDto.success(SuccessStatus.GET_PRAY_SUCCESS,
+			prayService.getPrayDetail(prayId, user.getUsername()));
+	}
 
-    @PostMapping
-    public ApiResponseDto<PrayResponseDto> createPray(
-        @RequestBody @Valid PrayRequestDto prayRequestDto,
-        @AuthenticationPrincipal User user
-    ) {
-        return ApiResponseDto.success(SuccessStatus.CREATE_PRAY_SUCCESS,
-            prayFacade.createPray(prayRequestDto, user.getUsername(), null));
-    }
+	@PostMapping
+	public ApiResponseDto<PrayResponseDto> createPray(
+		@RequestBody @Valid PrayRequestDto prayRequestDto,
+		@AuthenticationPrincipal User user
+	) {
+		return ApiResponseDto.success(SuccessStatus.CREATE_PRAY_SUCCESS,
+			prayFacade.createPray(prayRequestDto, user.getUsername(), null));
+	}
 
-    @DeleteMapping("/{prayId}")
-    public ApiResponseDto<PrayResponseDto> deletePray(
-        @PathVariable("prayId") Long prayId,
-        @AuthenticationPrincipal User user
-    ) {
-        return ApiResponseDto.success(SuccessStatus.DELETE_PRAY_SUCCESS,
-            prayFacade.deletePray(prayId, user.getUsername()));
-    }
+	@DeleteMapping("/{prayId}")
+	public ApiResponseDto<PrayResponseDto> deletePray(
+		@PathVariable("prayId") Long prayId,
+		@AuthenticationPrincipal User user
+	) {
+		return ApiResponseDto.success(SuccessStatus.DELETE_PRAY_SUCCESS,
+			prayFacade.deletePray(prayId, user.getUsername()));
+	}
 
-    @PutMapping("/{prayId}")
-    public ApiResponseDto<PrayResponseDto> updatePray(
-        @PathVariable("prayId") Long prayId,
-        @RequestBody @Valid PrayUpdateRequestDto prayUpdateRequestDto,
-        @AuthenticationPrincipal User user
-    ) {
-        return ApiResponseDto.success(SuccessStatus.UPDATE_PRAY_SUCCESS,
-            prayFacade.updatePray(prayId, user.getUsername(), prayUpdateRequestDto));
-    }
+	@PutMapping("/{prayId}")
+	public ApiResponseDto<PrayResponseDto> updatePray(
+		@PathVariable("prayId") Long prayId,
+		@RequestBody @Valid PrayUpdateRequestDto prayUpdateRequestDto,
+		@AuthenticationPrincipal User user
+	) {
+		return ApiResponseDto.success(SuccessStatus.UPDATE_PRAY_SUCCESS,
+			prayFacade.updatePray(prayId, user.getUsername(), prayUpdateRequestDto));
+	}
 
-    @PutMapping("/{prayId}/today")
-    public ApiResponseDto<List<PrayListResponseDto>> todayPray(
-        @PathVariable("prayId") Long prayId,
-        @AuthenticationPrincipal User user
-    ) {
-        return ApiResponseDto.success(SuccessStatus.INCREASE_PRAY_COUNT_SUCCESS,
-            prayFacade.todayPray(prayId, user.getUsername()));
-    }
+	@PutMapping("/{prayId}/today")
+	public ApiResponseDto<List<PrayListResponseDto>> todayPray(
+		@PathVariable("prayId") Long prayId,
+		@AuthenticationPrincipal User user
+	) {
+		return ApiResponseDto.success(SuccessStatus.INCREASE_PRAY_COUNT_SUCCESS,
+			prayFacade.todayPray(prayId, user.getUsername()));
+	}
 
-    @PutMapping("/{prayId}/complete")
-    public ApiResponseDto<List<PrayListResponseDto>> completePray(
-        @PathVariable("prayId") Long prayId,
-        @AuthenticationPrincipal User user
-    ) {
-        return ApiResponseDto.success(SuccessStatus.GET_PRAY_LIST_SUCCESS,
-            prayFacade.completePray(prayId, user.getUsername()));
-    }
+	@PutMapping("/{prayId}/complete")
+	public ApiResponseDto<List<PrayListResponseDto>> completePray(
+		@PathVariable("prayId") Long prayId,
+		@AuthenticationPrincipal User user
+	) {
+		return ApiResponseDto.success(SuccessStatus.GET_PRAY_LIST_SUCCESS,
+			prayFacade.completePray(prayId, user.getUsername()));
+	}
 
-    @PutMapping("/{prayId}/cancel")
-    public ApiResponseDto<List<PrayListResponseDto>> cancelPray(
-        @PathVariable("prayId") Long prayId,
-        @AuthenticationPrincipal User user
-    ) {
-        return ApiResponseDto.success(SuccessStatus.CANCEL_PRAY_SUCCESS,
-            prayFacade.cancelPray(prayId, user.getUsername()));
-    }
+	@PutMapping("/{prayId}/cancel")
+	public ApiResponseDto<List<PrayListResponseDto>> cancelPray(
+		@PathVariable("prayId") Long prayId,
+		@AuthenticationPrincipal User user
+	) {
+		return ApiResponseDto.success(SuccessStatus.CANCEL_PRAY_SUCCESS,
+			prayFacade.cancelPray(prayId, user.getUsername()));
+	}
 }
